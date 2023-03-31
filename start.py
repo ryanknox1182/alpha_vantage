322a48d028c5b43299e1de3d92c6e2a8c85a6dd3
ryanknox1182 <129538118+ryanknox1182@users.noreply.github.com>
2023-03-31 13:22:58 -0500

    Update start.py

--- start.py
+++ start.py
@@ -2,7 +2,7 @@ from alpha_vantage.timeseries import TimeSeries
 import matplotlib.pyplot as plt
 
 apikey = os.environ['ALPHAVANTAGE_API_KEY']
-ts = TimeSeries(key={apikey}, output_format='pandas')
+ts = TimeSeries(key='QUE4JESTDHAN1V5N', output_format='pandas')
 data, meta_data = ts.get_intraday(symbol='SPY',interval='5min', outputsize='full')
 data['4. close'].plot()
 plt.title('Intraday Times Series for the MSFT stock (1 min)')
